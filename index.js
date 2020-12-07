48c1756a75dd0d3a2f3f8f6086183cafba6bdfb6
Miika <mhtoiv@utu.fi>
2020-12-07 09:13:48 +0200

    cleaned code up

--- index.js
+++ index.js
@@ -6,56 +6,41 @@ const maxprice = Number(process.argv[2]);
 const displayPrice = process.argv[3] === 'y';
 
 
-const urls = require('./urls').urls;
+const urls = require('./constants').urls;
 
 const check = async(url) => {
+    let title;
+    let price;
+    let stock;
         await axios
             .get(url)
             .then(res => {
                 let $ = cheerio.load(res.data);
                 if (url.includes("verkkokauppa.com")) {
-                    const title = $('.product-header-title').text();
-                    const price = $('.price-tag-content__price-tag-price--current').text().replace(/\s+/g, "").replace(",", ".");
-                    const stock = $('.status') === 'status-green';
+                    title = $('.product-header-title').text();
+                    price = $('.price-tag-content__price-tag-price--current').text().replace(/\s+/g, "").replace(",", ".");
+                    stock = $('.status') === 'status-green';
                     console.log("Verkkokauppa.com: " + title);
-                    if (displayPrice && maxprice >= Number(price)) {
-                        console.log(chalk.green(price + "€"));
-                    } else if (displayPrice) {
-                        console.log(chalk.red(price + "€"));
-                    }
-                    if (stock) {
-                        console.log(chalk.green("ON STOCK!!!"))
-                        if (maxprice >= price) {
-                            console.log(chalk.green(price + "€!!!"));
-                        } else {
-                            console.log(chalk.red(price + "€ EXCEEDS MAX PRICE"));
-                        }
-                    } else {
-                        console.log(chalk.red("OUT OF STOCK"));
-                    }
                 } else if (url.includes("jimms.fi")) {
-                    const title = $('.name:nth-child(1)').text();
-                    const price = $('.pricetext>span').text().replace(/\s+/g, "").replace(",", ".").replace("€", "");
-                    const stock = $('.whrow:nth-child(2)>.whqty').text() !== '0 kpl';
+                    title = $('.name:nth-child(1)').text();
+                    price = $('.pricetext>span').text().replace(/\s+/g, "").replace(",", ".").replace("€", "");
+                    stock = $('.whrow:nth-child(2)>.whqty').text() !== '0 kpl';
                     console.log("Jimm's: " + title);
-                    if (displayPrice && maxprice >= Number(price)) {
-                        console.log(chalk.green(price + "€"));
-                    } else if (displayPrice) {
-                        console.log(chalk.red(price + "€"));
-                    }
-                    if (stock) {
-                        console.log(chalk.green("ON STOCK!!!"))
-                        console.log("price: " + price + " maxprice: " + maxprice)
-                        if (maxprice >= price) {
-                            console.log(chalk.green(price + "€!!!"));
-                        } else {
-                            console.log(chalk.red(price + "€ EXCEEDS MAX PRICE"));
-                        }
+                }
+                if (displayPrice && maxprice >= Number(price)) {
+                    console.log(chalk.green(price + "€"));
+                } else if (displayPrice) {
+                    console.log(chalk.red(price + "€"));
+                }
+                if (stock) {
+                    console.log(chalk.green("ON STOCK!!!"))
+                    if (maxprice >= price) {
+                        console.log(chalk.green(price + "€!!!"));
                     } else {
-                        console.log(chalk.red("OUT OF STOCK"));
+                        console.log(chalk.red(price + "€ EXCEEDS MAX PRICE"));
                     }
-
-
+                } else {
+                    console.log(chalk.red("OUT OF STOCK"));
                 }
             })
             .catch(error => console.log(error));

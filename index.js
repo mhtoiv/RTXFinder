0abdea6ea6bf8e65ca98951e56b411bdedb6cfbc
Miika Toivanen <miika.h.toivanen@utu.fi>
2020-12-10 08:30:33 +0200

    cleaned code up

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
-const cheerio = require("cheerio");
+const cheerio = require('cheerio');
 const axios = require('axios');
-const chalk = require("chalk");
+const chalk = require('chalk');
 
-const maxprice = Number(process.argv[2]);
+const maxPrice = Number(process.argv[2]);
 const displayPrice = process.argv[3] === 'y';
-
 
 const urls = require('./constants').urls;
 
 const check = async(url) => {
-    let title;
-    let price;
-    let stock;
+    let productTitle;
+    let productPrice;
+    let stockStatus;
+
         await axios
             .get(url)
             .then(res => {
                 let $ = cheerio.load(res.data);
                 if (url.includes("verkkokauppa.com")) {
-                    title = $('.product-header-title').text();
-                    price = $('.price-tag-content__price-tag-price--current').text().replace(/\s+/g, "").replace(",", ".");
-                    stock = $('.status').attr('class') === 'status status--green';
-                    console.log("Verkkokauppa.com: " + title);
+                    productTitle = $('.product-header-title').text();
+                    productPrice = $('.price-tag-content__price-tag-price--current').text()
+                        .replace(/\s+/g, "")
+                        .replace(",", ".");
+                    stockStatus = $('.status').attr('class') === 'status status--green';
+                    console.log("Verkkokauppa.com: " + productTitle);
                 } else if (url.includes("jimms.fi")) {
-                    title = $('.name:nth-child(1)').text();
-                    price = $('.pricetext>span').text().replace(/\s+/g, "").replace(",", ".").replace("€", "");
-                    stock = !$('.deliverytime').text().includes("Ei vahvistettu")
-                    console.log("Jimm's: " + title);
-
+                    productTitle = $('.name:nth-child(1)').text();
+                    productPrice = $('.pricetext>span').text()
+                        .replace(/\s+/g, "")
+                        .replace(",", ".")
+                        .replace("€", "");
+                    stockStatus = !$('.deliverytime').text().includes("Ei vahvistettu")
+                    console.log("Jimm's: " + productTitle);
                 }
-                if (displayPrice && maxprice >= Number(price)) {
-                    console.log(chalk.green(price + "€"));
+                if (displayPrice && maxPrice >= Number(productPrice)) {
+                    console.log(chalk.green(productPrice + "€"));
                 } else if (displayPrice) {
-                    console.log(chalk.red(price + "€"));
+                    console.log(chalk.red(productPrice + "€"));
                 }
-                if (stock) {
-                    console.log(chalk.green("ON STOCK!!!"))
-                    if (maxprice >= price) {
-                        console.log(chalk.green(price + "€!!!"));
+                if (stockStatus) {
+                    console.log(chalk.green("ON STOCK!!!"));
+                    if (maxPrice >= productPrice) {
+                        console.log(chalk.green(productPrice + "€!!!"));
                     } else {
-                        console.log(chalk.red(price + "€ EXCEEDS MAX PRICE"));
+                        console.log(chalk.red(productPrice + "€ EXCEEDS MAX PRICE"));
                     }
                 } else {
                     console.log(chalk.red("OUT OF STOCK"));
                 }
             })
             .catch(error => console.log(error));
-}
+};
 
 const startSearch = async() => {
     for (let i = 0; i < urls.length; i++) {
-        await check(urls[i])
-
+        await check(urls[i]);
     }
-}
+};
 
 startSearch().then(() => console.log("Checkup completed!"));
 
